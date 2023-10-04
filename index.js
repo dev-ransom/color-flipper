fc333409b9814f694afdf74361d7d34c82ef7b77
idungstanley <sundaystanley56@gmail.com>
2023-10-04 12:22:09 +0100

    added code for clear btn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 /** SELECT ITEMS */
-const submitBtn = document.querySelector('#submitBtn'),itemInput = document.querySelector('#itemInput'), grocery_form = document.querySelector('.grocery_form'), grocery_list = document.querySelector('.grocery_list'),grocery_container = document.querySelector('.grocery_container'),grocery_item = document.querySelector('.grocery_item'), alert = document.querySelector('.alert'), delete_btn = document.querySelector('.delete_btn'), clear_btn = document.querySelector('.clear_btn');
+const submitBtn = document.querySelector('#submitBtn'),itemInput = document.querySelector('#itemInput'), grocery_form = document.querySelector('.grocery_form'), grocery_list = document.querySelector('.grocery_list'), grocery_container = document.querySelector('.grocery_container'),grocery_item = document.querySelector('.grocery_item'), alert = document.querySelector('.alert'), delete_btn = document.querySelector('.delete_btn'), clear_btn = document.querySelector('.clear_btn');
 
 // ******************** edit options *************
 let editElement;
@@ -9,7 +9,7 @@ let editID = "";
 
 // Eventlistners 
 grocery_form.addEventListener('submit', addItem);
-
+clear_btn.addEventListener('click', clearItems);
 // function
 
 function addItem(e){
@@ -27,9 +27,24 @@ function addItem(e){
         const Attr = document.createAttribute('data-id');
         Attr.value = id;
         element.setAttributeNode(Attr);
-        console.log(element);
+        element.innerHTML = `<p class="item">${value}</p>
+        <article class="btn_container">
+            <button class="edit_btn">
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button class="delete_btn">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </article>`
+        grocery_list.appendChild(element)
         // displayAlert
         displayAlert('item added to the list', 'success', 'visible')
+        // show cotainer
+        grocery_container.classList.add('show_container');
+        // add to locale storage
+        addToLocalStorage(id,value);
+        // set back to default 
+        setBackToDefault();
     }
     else if( value  && editFlag ){
         
@@ -40,6 +55,17 @@ function addItem(e){
     
 }
 
+// clearItems
+function clearItems(){
+    const items = document.querySelectorAll('.grocery_item');
+    if(items.length > 0){
+        items.forEach(function(item){
+            grocery_list.removeChild(item);
+        })
+    }
+    grocery_container.classList.remove('show_container');
+    displayAlert('empty list', 'danger', 'visible')
+}
 // displayAlert function
 
 function displayAlert(text, action, visible){
@@ -55,6 +81,19 @@ function displayAlert(text, action, visible){
        },1000)
 }
 
-// local storage 
 
+
+// set back to default 
+function setBackToDefault(){
+    itemInput.value = '';
+    editFlag = false;
+    editID = '';
+    submitBtn.textContent = 'Submit'
+}
+
+// local storage 
+function addToLocalStorage(id, value){
+    console.log('added to local storage');
+}
 // select items
+
